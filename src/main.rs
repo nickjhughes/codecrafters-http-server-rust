6c33f7253aa0d7430b027d803877778a52668a40
nickjhughes <689543+nickjhughes@users.noreply.github.com>
2023-11-07 20:41:11 +1100

    Parse request

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,39 @@
-use std::{io::Write, net::TcpListener};
+use anyhow::Result;
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
+use request::Request;
 use response::Response;
 use status_code::StatusCode;
 
-// mod method;
-// mod request;
+mod method;
+mod request;
 mod response;
 mod status_code;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:4221")?;
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                let response = Response::from_status_code(StatusCode::OK);
-                stream.write_all(&response.encode().unwrap()).unwrap();
+                let mut buf = Vec::new();
+                stream.read_to_end(&mut buf)?;
+                let request = Request::parse(&buf)?;
+                let response = if request.target == "/" {
+                    Response::from_status_code(StatusCode::OK)
+                } else {
+                    Response::from_status_code(StatusCode::NotFound)
+                };
+                stream.write_all(&response.encode()?)?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
